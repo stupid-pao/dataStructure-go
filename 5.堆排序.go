c0e3812d1290bb015de68e5fa8e85258fd30714c
XQ <dahuaciliu@gmail.com>
2020-03-01 22:20:04 +0800

    .

---
+++ 5.堆排序.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+二叉树跟和孩子节点的 索引的关系
+0             a[n]
+
+
+1     a[2*n+1]    a[2*n+2]
+
+
+
+*/
+
+func HeapGetMax(arr []int) int {
+	length := len(arr)
+	if length <= 1 {
+		return arr[0]
+	} else {
+
+	}
+}
+
+func main() {
+	arr := []int{1, 9, 2, 8, 3, 7, 4, 6, 5, 10}
+}
